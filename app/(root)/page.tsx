48adec6f36354b26a14a26bcbd7eed06ac5f7315
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-10-16 22:04:06 +0300

    Fetch and display events in the Home component using getAllEvents function

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,19 @@
+import Collection from "@/components/shared/Collection";
 import { Button } from "@/components/ui/button";
+import { getAllEvents } from "@/lib/actions/event.actions";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  
+  const events = await getAllEvents({
+    query: "",
+    category: "",
+    page: 1,
+    limit: 6,
+  })
+
+
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
@@ -25,11 +36,11 @@ export default function Home() {
           </div>
 
           <Image
-          src="/assets/images/hero.png"
-          alt="hero"
-          width={1000}
-          height={1000}
-          className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
+            src="/assets/images/hero.png"
+            alt="hero"
+            width={1000}
+            height={1000}
+            className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
           />
 
         </div>
@@ -37,14 +48,23 @@ export default function Home() {
       </section>
 
       <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
-        <h2 className="h2-bold">Trusted by <br/> Thousands of Events</h2>
+        <h2 className="h2-bold">Trusted by <br /> Thousands of Events</h2>
 
         <div className="flex w-full flex-col gap-5 md:flex-row">
           Search
           CategoryFilter
         </div>
+        <Collection
+          data={events?.data}
+          emptyTitle="No events found"
+          emptyStateSubtext="Come back Later :)"
+          collectionType="All_Events"
+          limit={6}
+          page={1}
+          totalPages={2}
+        />
       </section>
-      
+
     </>
   );
 }
