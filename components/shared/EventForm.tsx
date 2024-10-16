402b338b7b14ee9f2972d36ac5857978841115a7
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-10-16 12:14:12 +0300

    Add price and URL fields to EventForm with isFree checkbox

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -22,6 +22,8 @@ import { useState } from 'react';
 import Image from 'next/image';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { Label } from '../ui/label';
+import { Checkbox } from '../ui/checkbox';
 
 
 
@@ -52,7 +54,7 @@ const EventForm = ({ userId, type }: EventFormProps) => {
 
                 <div className='flex flex-col gap-5 md:flex-row'>
 
-                // Title
+                    {/* // Title */}
                     <FormField
                         control={form.control}
                         name="title"
@@ -66,7 +68,7 @@ const EventForm = ({ userId, type }: EventFormProps) => {
                         )}
                     />
 
-                    // Category
+                    {/* // Category */}
                     <FormField
                         control={form.control}
                         name="categoryId"
@@ -86,7 +88,7 @@ const EventForm = ({ userId, type }: EventFormProps) => {
 
                 <div className='flex flex-col gap-5 md:flex-row'>
 
-                    // Description
+                    {/* // Description */}
                     <FormField
                         control={form.control}
                         name="description"
@@ -100,7 +102,7 @@ const EventForm = ({ userId, type }: EventFormProps) => {
                         )}
                     />
 
-                    // Image
+                    {/* // Image */}
                     <FormField
                         control={form.control}
                         name="imageUrl"
@@ -118,8 +120,8 @@ const EventForm = ({ userId, type }: EventFormProps) => {
                         )}
                     />
                 </div>
-                
-                // Location
+
+                {/* // Location */}
                 <div className='flex flex-col gap-5 md:flex-row'>
                     <FormField
                         control={form.control}
@@ -147,7 +149,7 @@ const EventForm = ({ userId, type }: EventFormProps) => {
 
                 <div className='flex flex-col gap-5 md:flex-row'>
 
-                    // Start Date
+                    {/* // Start Date */}
                     <FormField
                         control={form.control}
                         name="startDateTime"
@@ -178,7 +180,7 @@ const EventForm = ({ userId, type }: EventFormProps) => {
                         )}
                     />
 
-                    // End Date
+                    {/* // End Date */}
                     <FormField
                         control={form.control}
                         name="endDateTime"
@@ -210,7 +212,95 @@ const EventForm = ({ userId, type }: EventFormProps) => {
                     />
                 </div>
 
-                <Button type="submit">Submit</Button>
+                <div className='flex flex-col gap-5 md:flex-row'>
+
+                    {/* // Price */}
+                    <FormField
+                        control={form.control}
+                        name="price"
+                        render={({ field }) => (
+                            <FormItem className='w-full'>
+                                <FormControl>
+                                    <div className='flex-center h-[54px] w-full overflow-hidden rounded-full bg-gray-50 px-4 py-2'>
+                                        <Image
+                                            src="/assets/icons/dollar.svg"
+                                            alt="dollar"
+                                            width={24}
+                                            height={24}
+                                            className='filter-grey'
+                                        />
+                                        <Input type="number"
+                                            placeholder="Price"
+                                            {...field}
+                                            className='p-regular-16 border-0 bg-green-50 outline-offset-0 focus:border-o focus-visible:ring-0 focus-visible:ring-offset-0'
+                                        />
+
+                                        {/* // isFree */}
+                                        <FormField
+                                            control={form.control}
+                                            name="isFree"
+                                            render={({ field }) => (
+                                                <FormItem>
+                                                    <FormControl>
+                                                        <div className='flex items-center'>
+                                                            <Label htmlFor='isFree' className='whitespace-nowrap pr-3 leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 font-bold'>Free Ticket</Label>
+                                                            <Checkbox
+                                                                id='isFree'
+                                                                onCheckedChange={field.onChange}
+                                                                checked={field.value}
+                                                                className='mr-2 h-5 w-5 border-2 border-primary-500' />
+                                                        </div>
+                                                    </FormControl>
+                                                    <FormMessage />
+                                                </FormItem>
+                                            )}
+                                        />
+
+                                    </div>
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+                    {/* url */}
+                    <FormField
+                        control={form.control}
+                        name="url"
+                        render={({ field }) => (
+                            <FormItem className='w-full'>
+                                <FormControl>
+                                    <div className='flex-center h-[54px] w-full overflow-hidden rounded-full bg-gray-50 px-4 py-2'>
+                                        <Image
+                                            src="/assets/icons/link.svg"
+                                            alt="link"
+                                            width={24}
+                                            height={24}
+                                        />
+
+                                        <Input placeholder="url" {...field} className='input-field' />
+
+                                    </div>
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+                </div>
+
+                <Button
+                    type="submit"
+                    size='lg'
+                    disabled={form.formState.isSubmitting}
+                    className='button col-span-2 w-full'
+                >
+                    {
+                        form.formState.isSubmitting ? 'Submitting...'
+                            :
+                            `${type} Event`
+                    }
+                </Button>
 
             </form>
         </Form>
