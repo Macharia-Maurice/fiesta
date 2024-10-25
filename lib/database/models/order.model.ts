6a6cc95f3c840efc7171aab8418f02d7a5721925
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-10-25 20:44:47 +0300

    Rename stripeId to transactionId in Order model for clarity

--- lib/database/models/order.model.ts
+++ lib/database/models/order.model.ts
@@ -2,7 +2,7 @@ import { Schema, model, models, Document } from 'mongoose'
 
 export interface IOrder extends Document {
     createdAt: Date
-    stripeId: string
+    transactionId: string
     totalAmount: string
     event: {
         _id: string
@@ -26,7 +26,7 @@ export type IOrderItem = {
 
 const OrderSchema = new Schema({
     createdAt: { type: Date, default: Date.now },
-    stripeId: { type: String, required: true, unique: true },
+    transactionId: { type: String, required: true, unique: true },
     totalAmount: { type: String },
     event: { type: Schema.Types.ObjectId, ref: 'Event' },
     buyer: { type: Schema.Types.ObjectId, ref: 'User' },
