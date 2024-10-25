b15007c4514f7003311849fd8b75bfe90d215db9
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-10-25 20:44:27 +0300

    Rename CheckOut component to Checkout and update checkout function for order processing

--- components/shared/CheckOut.tsx
+++ components/shared/CheckOut.tsx
@@ -2,12 +2,20 @@ import React from 'react'
 import { Button } from '../ui/button';
 import { IEvent } from '@/lib/database/models/event.model';
 
-const CheckOut = ({ event, userId }: { event: IEvent, userId: string }) => {
-    const onCheckOut = async () => {
-        console.log('Checking out');
+const Checkout = ({ event, userId }: { event: IEvent, userId: string }) => {
+    const onCheckout = async () => {
+        const order ={
+            eventTitle: event.title,
+            eventId: event._id,
+            buyerId: userId,
+            isFree: event.isFree,
+            price: event.price,
+        }
+
+        // await checkoutOrder(order);
     }
     return (
-        <form action={onCheckOut} method='post'>
+        <form action={onCheckout} method='post'>
             <Button
             type='submit'
             role='link'
@@ -20,4 +28,4 @@ const CheckOut = ({ event, userId }: { event: IEvent, userId: string }) => {
     )
 }
 
-export default CheckOut
+export default Checkout
