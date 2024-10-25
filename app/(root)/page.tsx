c5b2f1a0c0db11934a63357fc54f31cb3c50a0a5
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-10-25 23:47:55 +0300

    Update page component to use dynamic page number for event display

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -66,7 +66,7 @@ export default async function Home({ searchParams}: SearchParamProps) {
           emptyStateSubtext="Come back Later :)"
           collectionType="All_Events"
           limit={6}
-          page={1}
+          page={page}
           totalPages={events?.totalPages}
         />
       </section>
